6f9ed02f9b68e785850aca288dc3c11acd1d4cfc
David Crawford <206953920+1NobleCyber@users.noreply.github.com>
2026-08-07 21:23:55 -0500

    Delete tests directory

--- tests/test_client.py
+++
@@ -1,5 +0,0 @@
-def test_client_connection(iboss_client):
-    assert iboss_client.auth_token is not None
-    assert "Gateway" in iboss_client.domains
-    assert "Reporting" in iboss_client.domains
-    assert iboss_client.gateway_version is not None
--- tests/test_groups.py
+++
@@ -1,8 +0,0 @@
-from pyiboss.groups import get_iboss_group
-
-def test_groups(iboss_client):
-    groups = get_iboss_group(iboss_client, maximum_items_to_return=5)
-    assert isinstance(groups, list)
-    if groups:
-        assert "GroupName" in groups[0]
-        assert "GroupId" in groups[0]
